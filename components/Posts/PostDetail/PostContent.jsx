858007518f8a3ca71870f877d640978c34d1798b
Roman Shishkin <roman.shishkin87@gmail.com>
2021-06-03 11:39:35 +0600

    react markdown configured, syntax highlight added

--- components/Posts/PostDetail/PostContent.jsx
+++ components/Posts/PostDetail/PostContent.jsx
@@ -1,23 +1,64 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './PostHeader';
 
 import styles from './PostContent.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started with NextJS',
-  image: 'getting-started-nextjs.png',
-  date: '2021-05-30',
-  content: '# This is a first post',
-};
+const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const customComponents = {
+    // img(image) {
+    //   return (
+    //     <Image 
+    //       src={`/images/posts/${post.slug}/${image.src}`} 
+    //       alt={image.alt} 
+    //       width={600} 
+    //       height={300}
+    //     />
+    //   )
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+             <Image 
+                src={`/images/posts/${post.slug}/${image.properties.src}`} 
+                alt={image.alt} 
+                width={600} 
+                height={300}
+             />
+          </div>
+        )
+      }
+
+      return <p>{paragraph.children}</p>
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      
+      return (
+        <SyntaxHighlighter 
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    }
+  };
+
   return (
     <article className={styles.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customComponents}>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,7 +1,34 @@
 import PostContent from '../../components/Posts/PostDetail/PostContent';
 
-const PostDetailPage = () => (
-  <PostContent />
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
+
+const PostDetailPage = ({ post }) => (
+  <PostContent post={post} />
 );
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+      ravalidate: 600,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
+};
 
 export default PostDetailPage;
